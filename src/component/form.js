154f5c64e999ca6acf2dd6e8113c69faff48ae73
Nhi <nhipham0902@gmail.com>
2024-02-02 12:59:26 +0100

    add final message and close button

--- src/component/form.js
+++ src/component/form.js
@@ -20,29 +20,26 @@ const phone = document.getElementById("phone");
 const isAdult = document.getElementById("isAdult");
 const submitButton = document.querySelector(".submit");
 
-const submitData = () => {
-  const submitDataFromForm = async () => {
-    const url = `https://test-api.codingbootcamp.cz/api/2a8117b3/events/1/registrations`;
-    const myDataObject = {
-      firstname: `${fname.value}`,
-      surname: `${surname.value}`,
-      email: `${email.value}`,
-      phone: `${phone.value}`,
-      adult: `${isAdult}`,
-    };
-    const myResponse = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify(myDataObject),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const myUsableResponse = await myResponse.json();
-    console.log(myUsableResponse);
-    resultPage(myUsableResponse);
-    console.log(myUsableResponse);
+const submitData = async () => {
+  const url = `https://test-api.codingbootcamp.cz/api/2a8117b3/events/1/registrations`;
+  const myDataObject = {
+    firstname: `${fname.value}`,
+    surname: `${surname.value}`,
+    email: `${email.value}`,
+    phone: `${phone.value}`,
+    adult: `${isAdult}`,
   };
-  submitDataFromForm();
+  const myResponse = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(myDataObject),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const myUsableResponse = await myResponse.json();
+  console.log(myUsableResponse);
+  resultPage(myUsableResponse);
+  console.log(myUsableResponse);
 };
 
 submitButton.addEventListener("click", (event) => {
@@ -62,13 +59,21 @@ const closeModal = () => {
 
 const resultMessage = document.createElement("div");
 resultMessage.classList.add("resultMessage");
+const closeResult = document.createElement("button");
+closeResult.classList.add("closeResult");
+closeResult.textContent = "Close";
+closeResult.addEventListener("click", (event) => {
+  modalWindow.removeChild(resultMessage);
+  modalWindow.removeChild(closeResult);
+});
 
 const resultPage = (myUsableResponse) => {
   modalContent.style.display = "none";
   if (!myUsableResponse.message) {
     resultMessage.textContent = "Registered!";
   } else {
-    resultMessage.textContent = `${myUsableResponse.message}`;
+    resultMessage.textContent = `${myUsableResponse.message}. Please try again`;
   }
   modalWindow.appendChild(resultMessage);
+  modalWindow.appendChild(closeResult);
 };
